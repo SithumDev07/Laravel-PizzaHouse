2dd403cd18660ed50620d33b5edc2b2075c7da4a
Sithum Basnayaka <sithum.dashantha@gmail.com>
2021-06-25 12:48:14 +0530

    added Delete method to delete records

--- resources/views/pizzas/show.blade.php
+++ resources/views/pizzas/show.blade.php
@@ -3,15 +3,20 @@
 @section('content')
    
     <div class="wrapper pizza-details">
-        <h1>Order for {{ $pizza->name }}</h1>
+        <h1>Order for {{ $pizza->name }}</h1> 
         <p class="type">Type is - {{ $pizza->type }}</p>
         <p class="base">Type is - {{ $pizza->base }}</p>
         <p class="toppings">Extra toppings:</p>
         <ul>
             @foreach($pizza->toppings as $topping)
-                <li>{{ $topping }}</li>
+                <li>{{ $topping }}</li> 
             @endforeach
         </ul>
+        <form action="/pizzas/{{ $pizza->id }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button>Complete Order</button>
+        </form>
     </div>
 
     <a href="/pizzas" class="back">Back to all pizzas</a>
